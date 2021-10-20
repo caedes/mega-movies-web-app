a0d1ae7f26426dd70c96aaf13b964a10130953ca
Romain Laurent <romain.laurent@cdiscount.com>
2021-10-20 17:48:44 +0200

    feat: 🎸 add empty HomePage

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import { CssBaseline } from "@material-ui/core";
+import HomePage from "../HomePage";
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <HomePage />
+    </>
+  );
+}
