03544f55cd4e18ea128f70e6bc1b9ae8d60ab591
Romain Laurent <romain.laurent@cdiscount.com>
2021-10-26 11:04:10 +0200

    feat: 🎸 design the MenuItem components

--- src/ds/BaseLayout/index.js
+++ src/ds/BaseLayout/index.js
@@ -1,5 +1,7 @@
 import { node } from "prop-types";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { HomeOutlined, ListOutlined, SearchOutlined } from "@material-ui/icons";
+
 import MenuItem from "../MenuItem";
 import Separator from "../Separator";
 
@@ -9,9 +11,9 @@ export default function BaseLayout({ children }) {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">Mega Movies</Typography>
-          <MenuItem>Accueil</MenuItem>
-          <MenuItem>Recherche</MenuItem>
-          <MenuItem>Ma liste</MenuItem>
+          <MenuItem startIcon={<HomeOutlined />}>Accueil</MenuItem>
+          <MenuItem startIcon={<SearchOutlined />}>Recherche</MenuItem>
+          <MenuItem startIcon={<ListOutlined />}>Ma liste</MenuItem>
           <Separator />
           <MenuItem>Login</MenuItem>
         </Toolbar>
--- src/ds/MenuItem/index.js
+++ src/ds/MenuItem/index.js
@@ -1,10 +1,23 @@
 import { node } from "prop-types";
 import { Button } from "@material-ui/core";
 
-export default function MenuItem({ children }) {
-  return <Button color="inherit">{children}</Button>;
+import useStyles from "./useStyles";
+
+export default function MenuItem({ children, startIcon }) {
+  const classes = useStyles();
+
+  return (
+    <Button
+      color="inherit"
+      classes={{ root: classes.root }}
+      startIcon={startIcon}
+    >
+      {children}
+    </Button>
+  );
 }
 
 MenuItem.propTypes = {
   children: node,
+  startIcon: node,
 };
