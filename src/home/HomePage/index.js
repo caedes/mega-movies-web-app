6a7e99180a769f1886c6f664e8e86076dc54eeda
Romain Laurent <romain.laurent@cdiscount.com>
2021-10-27 10:35:40 +0200

    feat: 🎸 prepare HomePage to display movies list

--- src/home/HomePage/index.js
+++ src/home/HomePage/index.js
@@ -1,10 +1,28 @@
+import { Grid } from "@material-ui/core";
+import { v4 as uuid } from "uuid";
+import React from "react";
+
 import BaseLayout from "../../ds/BaseLayout";
 import MovieCard from "../../ds/MovieCard";
 
+const defaultMovies = new Array(20).fill(null).map(() => ({
+  id: uuid(),
+  title: "Titanic",
+  image: "https://api.lorem.space/image/movie?w=350&h=210",
+}));
+
 export default function HomePage() {
+  const [movies] = React.useState(defaultMovies);
+
   return (
     <BaseLayout>
-      <MovieCard title="Titanic" id={12} />
+      <Grid container spacing={2}>
+        {movies.map((movie) => (
+          <Grid item key={movie.id}>
+            <MovieCard {...movie} />
+          </Grid>
+        ))}
+      </Grid>
     </BaseLayout>
   );
 }
